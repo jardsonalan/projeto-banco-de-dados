7349b367d6db992e2f433238518238a0b68c77ef
Ian Galvão <137799663+Barr0ca@users.noreply.github.com>
2025-08-06 17:24:52 -0300

    feat: Trigger 2

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -1,4 +1,5 @@
 -- Trigger que é acionada sempre que um item é inserido em pedido
+-- Ela ocorre após a inserção
 CREATE OR REPLACE FUNCTION atualiza_estoque()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -13,4 +14,18 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER atualiza_estoque AFTER INSERT ON itens_pedidos
 FOR EACH ROW EXECUTE FUNCTION atualiza_estoque();
 
+-- Trigger que é acionada sempre há a tentativa de adicionar um item em pedido
+-- Ela ocorre antes da inserção
+CREATE OR REPLACE FUNCTION verifica_estoque()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF (SELECT quantidade_estoque FROM produtos WHERE id = NEW.produto_id) < NEW.quantidade THEN
+		RAISE EXCEPTION 'Estoque insuficiente para produto %', NEW.produto_id;
+	END IF;
 
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER verifica_estoque BEFORE INSERT ON itens_pedidos
+FOR EACH ROW EXECUTE FUNCTION verifica_estoque();
